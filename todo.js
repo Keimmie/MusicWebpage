e1a44dcdda201f48c034dabf208113c34d1f3bea
Keimmie <Keimmie@gmail.com>
2020-05-13 02:04:28 -0400

    update

--- todo.js
+++ todo.js
@@ -1,22 +1,14 @@
-var on = true;
-var myVar = setInterval(function(){ if(on) {slides()}on=true;},3000);
+var slideIndex = 0;
+showSlides();
 
-function slides(i) {
-  document.getElementsByTagName("div")[1].style.width="0px";
-  document.getElementsByTagName("div")[2].style.width="100%";
-  var slide = document.getElementsByTagName("div")[0];
-  document.getElementById("slide").appendChild(slide);
-  if(i) {
-   on = false;
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-}
-function back_slide(i) {
-  document.getElementsByTagName("div")[1].style.width="0px";
-  document.getElementsByTagName("div")[0].style.width="100%";
-  var slide = document.getElementsByTagName("div")[4];
-  var slide2 = document.getElementsByTagName("div")[0];
-  document.getElementById("slide").insertBefore(slide,slide2);
-  if(i) {
-   on = false;
-  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
 }
